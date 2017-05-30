543b63e1e79b2d865f38f87d8ecd6abe41afc174
svartephilip <ohaukeboe@gmail.com>
2017-05-30 15:12:59 +0200

    nytt design i activity_start

--- app/src/main/java/com/magnus/oskar/quiltingcalc/BackBatActivity.java
+++ app/src/main/java/com/magnus/oskar/quiltingcalc/BackBatActivity.java
@@ -1,8 +1,6 @@
 package com.magnus.oskar.quiltingcalc;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -13,17 +11,6 @@ public class BackBatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_back_bat);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
-
 }
