709d2e686e0224bfc961049c0498c3ffd3bd469b
svartephilip <ohaukeboe@gmail.com>
2017-06-12 23:16:46 +0200

    nytt design i activity_start

--- app/src/main/java/com/magnus/oskar/quiltingcalc/BackBatActivity.java
+++ app/src/main/java/com/magnus/oskar/quiltingcalc/BackBatActivity.java
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Spinner;
 
 public class BackBatActivity extends AppCompatActivity {
 
